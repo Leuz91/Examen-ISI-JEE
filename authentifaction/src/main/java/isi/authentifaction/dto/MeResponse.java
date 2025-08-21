7ccf8146b1609461ffedff4ec22c73122ff5e8e1
EL HADJI YORO GUEYE <83180430+Leuz91@users.noreply.github.com>
2025-08-21 18:48:14 +0000

    Delete authentifaction directory
    lol

--- authentifaction/src/main/java/isi/authentifaction/dto/MeResponse.java
+++
@@ -1,7 +0,0 @@
-package isi.authentifaction.dto;
-
-import java.util.Set;
-
-public record MeResponse(
-        Long id, String username, String email, Set<String> roles
-) {}
--- authentifaction/src/main/java/isi/authentifaction/dto/RefreshRequest.java
+++
@@ -1,7 +0,0 @@
-package isi.authentifaction.dto;
-
-import jakarta.validation.constraints.NotBlank;
-
-public record RefreshRequest(
-        @NotBlank String refreshToken
-) {}
