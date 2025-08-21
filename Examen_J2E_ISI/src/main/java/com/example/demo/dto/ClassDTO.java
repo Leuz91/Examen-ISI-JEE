aed1f10b173c7d8fe02e01a82551632e02b66794
EL HADJI YORO GUEYE <83180430+Leuz91@users.noreply.github.com>
2025-08-21 18:47:53 +0000

    Delete Examen_J2E_ISI directory
    lol

--- Examen_J2E_ISI/src/main/java/com/example/demo/dto/ClassDTO.java
+++
@@ -1,5 +0,0 @@
-package com.example.demo.dto;
-
-public record ClassDTO(Long id, String className, String description, Long sectorId) {
-
-}
--- Examen_J2E_ISI/src/main/java/com/example/demo/dto/SectorDTO.java
+++
@@ -1,5 +0,0 @@
-package com.example.demo.dto;
-
-public record SectorDTO(Long id, String name) {
-
-}
